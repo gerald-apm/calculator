0aa08ccb563b972ddb225ebc0d6005fdae6059ee
Geraldhi A. P. Mahayadnya <geraldiaditya1111@gmail.com>
2022-04-01 01:03:40 +0800

    added formatter

--- script.js
+++ script.js
@@ -1,158 +1,155 @@
-let prevNumber ='';
-let calculatorOperation ='';
-let currentNumber ='0';
-let equationStr = '';
+let prevNumber = "";
+let calculatorOperation = "";
+let currentNumber = "0";
+let equationStr = "";
 let startEq = false;
 
-const calculatorScreen = document.querySelector('.calculator-screen');
-const calculatorEquation = document.querySelector('.calculator-equation');
+const calculatorScreen = document.querySelector(".calculator-screen");
+const calculatorEquation = document.querySelector(".calculator-equation");
 
 const updateScreen = (number) => {
-    calculatorScreen.value = number;
+  calculatorScreen.value = number;
 };
 
 const updateEquation = (equation) => {
-    calculatorEquation.value = equation;
+  calculatorEquation.value = equation;
 };
 
 const inputNumber = (number) => {
-    if (currentNumber === prevNumber && startEq === false) {
-        currentNumber = '0';
-        startEq = true;
-    }
+  if (currentNumber === prevNumber && startEq === false) {
+    currentNumber = "0";
+    startEq = true;
+  }
 
-    if (currentNumber === '0'){
-        currentNumber = number;
-    } else {
-        currentNumber += number;
-    }
+  if (currentNumber === "0") {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  }
 };
 
 const inputEquation = (equation) => {
-    if (equationStr === '0'){
-        equationStr = equation;
-    } else {
-        equationStr += equation;
-    }
-}
-
-
-// operator
-const inputOperator = (operator) =>  {
-    if (calculatorOperation !== '') calculate();
-    calculatorOperation = operator;
-    prevNumber = currentNumber;
-    //currentNumber = '0';
-    startEq = false;
+  if (equationStr === "0") {
+    equationStr = equation;
+  } else {
+    equationStr += equation;
+  }
 };
 
+// operator
+const inputOperator = (operator) => {
+  if (calculatorOperation !== "") calculate();
+  calculatorOperation = operator;
+  prevNumber = currentNumber;
+  //currentNumber = '0';
+  startEq = false;
+};
 
-const inputDecimal = (dot) =>  {
-    if (currentNumber.includes(".")) return;
-    currentNumber += dot;
+const inputDecimal = (dot) => {
+  if (currentNumber.includes(".")) return;
+  currentNumber += dot;
 };
 
 const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((number) => {
-    number.addEventListener("click", (event) => {
-        // add event while clicked
-        inputNumber(event.target.value);
-        updateScreen(currentNumber);
-        inputEquation(currentNumber);
-        updateEquation(equationStr);
-        console.log(equationStr);
-    })   
+  number.addEventListener("click", (event) => {
+    // add event while clicked
+    inputNumber(event.target.value);
+    updateScreen(currentNumber);
+    inputEquation(currentNumber);
+    updateEquation(equationStr);
+    console.log(equationStr);
+  });
 });
 
 const operators = document.querySelectorAll(".operator");
 
 operators.forEach((operator) => {
-    operator.addEventListener("click", (event) => {
-        inputOperator(event.target.value);
-        updateScreen(event.target.value);
-        inputEquation(` ${event.target.value} `);
-        updateEquation(equationStr);
-    })   
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value);
+    updateScreen(event.target.value);
+    inputEquation(` ${event.target.value} `);
+    updateEquation(equationStr);
+  });
 });
 
 const equal = document.querySelector(".equal-sign");
 
 equal.addEventListener("click", () => {
-    calculate();
-    updateScreen(currentNumber);
-    inputEquation(` = ${currentNumber}`);
-    updateEquation(equationStr);
+  calculate();
+  updateScreen(currentNumber);
+  inputEquation(` = ${currentNumber}`);
+  updateEquation(equationStr);
 });
-
 
 const percentage = document.querySelector(".percentage");
 
 percentage.addEventListener("click", () => {
-    percentageNum();
-    updateScreen(currentNumber);
-    updateEquation(currentNumber);
+  percentageNum();
+  updateScreen(currentNumber);
+  updateEquation(currentNumber);
 });
 
 const clearer = document.querySelector(".all-clear");
 
 clearer.addEventListener("click", () => {
-    clearAll();
-    updateScreen(currentNumber);
-    inputEquation(currentNumber);
-    updateEquation(equationStr);
+  clearAll();
+  updateScreen(currentNumber);
+  inputEquation(currentNumber);
+  updateEquation(equationStr);
 });
 
 const decimal = document.querySelector(".decimal");
 
 decimal.addEventListener("click", (event) => {
-    inputDecimal(event.target.value);
-    updateScreen(currentNumber);
-    inputEquation(currentNumber);
-    updateEquation(equationStr);
+  inputDecimal(event.target.value);
+  updateScreen(currentNumber);
+  inputEquation(currentNumber);
+  updateEquation(equationStr);
 });
 
 const clearAll = () => {
-    currentNumber = '0';
-    prevNumber = '0';
-    equationStr = '';
-    calculatorOperation = '';
-    startEq = false;
+  currentNumber = "0";
+  prevNumber = "0";
+  equationStr = "";
+  calculatorOperation = "";
+  startEq = false;
 };
 
 const percentageNum = () => {
-    currentNumber = (parseFloat(currentNumber) / 100).toString();
-    prevNumber = currentNumber;
-    startEq = false;
+  currentNumber = (parseFloat(currentNumber) / 100).toString();
+  prevNumber = currentNumber;
+  startEq = false;
 };
 
 const calculate = () => {
-    let result = 0;
-    let number1 = parseFloat(prevNumber);
-    let number2 = parseFloat(currentNumber);
-    switch (calculatorOperation) {
-        case '+':
-            result = number1 + number2;
-            break;
-        case '-':
-            result = number1 - number2;
-            break;
-        case '*':
-            result = number1 * number2;
-            break;
-        case '/':
-            result = number1 / number2;
-            break;
-        default:
-            return;
-    }
+  let result = 0;
+  let number1 = parseFloat(prevNumber);
+  let number2 = parseFloat(currentNumber);
+  switch (calculatorOperation) {
+    case "+":
+      result = number1 + number2;
+      break;
+    case "-":
+      result = number1 - number2;
+      break;
+    case "*":
+      result = number1 * number2;
+      break;
+    case "/":
+      result = number1 / number2;
+      break;
+    default:
+      return;
+  }
 
-    if (result % parseInt(result) > 0) {
-        currentNumber = parseInt(result).toString();
-    } else {
-        currentNumber = result.toString();
-    }
-    prevNumber = currentNumber;
-    startEq = false;
-    calculatorOperation = '';
+  if (result % parseInt(result) > 0) {
+    currentNumber = parseInt(result).toString();
+  } else {
+    currentNumber = result.toString();
+  }
+  prevNumber = currentNumber;
+  startEq = false;
+  calculatorOperation = "";
 };
